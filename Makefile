254fc04ac956118d8ec0745c3112e5096c755c85
nadans <nada.nisrinaseptiana@homecredit.co.id>
2024-04-20 21:57:39 +0700

    init folder

---
+++ Makefile
@@ -0,0 +1,62 @@
+include .env
+
+help:
+	@echo "## docker-build			- Build Docker Images (amd64) including its inter-container network."
+	@echo "## docker-build-arm		- Build Docker Images (arm64) including its inter-container network."
+	@echo "## postgres			- Run a Postgres container  "
+	@echo "## clean			- Cleanup all running containers related to the challenge."
+
+docker-build:
+	@echo '__________________________________________________________'
+	@echo 'Building Docker Images ...'
+	@echo '__________________________________________________________'
+	@docker network inspect dataeng-network >/dev/null 2>&1 || docker network create dataeng-network
+	@echo '__________________________________________________________'
+	@docker build -t dataeng-dibimbing/spark -f ./docker/Dockerfile.spark .
+	@echo '__________________________________________________________'
+	@docker build -t dataeng-dibimbing/airflow -f ./docker/Dockerfile.airflow .
+	@echo '__________________________________________________________'
+	@docker build -t dataeng-dibimbing/jupyter -f ./docker/Dockerfile.jupyter .
+	@echo '==========================================================='
+
+docker-build-arm:
+	@echo '__________________________________________________________'
+	@echo 'Building Docker Images ...'
+	@echo '__________________________________________________________'
+	@docker network inspect dataeng-network >/dev/null 2>&1 || docker network create dataeng-network
+	@echo '__________________________________________________________'
+	@docker build -t dataeng-dibimbing/spark -f ./docker/Dockerfile.spark .
+	@echo '__________________________________________________________'
+	@docker build -t dataeng-dibimbing/airflow -f ./docker/Dockerfile.airflow-arm .
+	@echo '__________________________________________________________'
+	@docker build -t dataeng-dibimbing/jupyter -f ./docker/Dockerfile.jupyter .
+	@echo '==========================================================='
+
+
+airflow:
+	@echo '__________________________________________________________'
+	@echo 'Creating Airflow Instance ...'
+	@echo '__________________________________________________________'
+	@docker-compose -f ./docker/docker-compose-airflow.yml --env-file .env up
+	@echo '==========================================================='
+
+postgres: postgres-create 
+
+postgres-create:
+	@docker-compose -f ./docker/docker-compose-postgres.yml --env-file .env up -d
+	@echo '__________________________________________________________'
+	@echo 'Postgres container created at port ${POSTGRES_PORT}...'
+	@echo '__________________________________________________________'
+	@echo 'Postgres Docker Host	: ${POSTGRES_CONTAINER_NAME}' &&\
+		echo 'Postgres Account	: ${POSTGRES_USER}' &&\
+		echo 'Postgres password	: ${POSTGRES_PASSWORD}' &&\
+		echo 'Postgres Db		: ${POSTGRES_DW_DB}'
+	@sleep 5
+	@echo '==========================================================='
+
+clean:
+	@bash ./scripts/goodnight.sh
+
+
+postgres-bash:
+	@docker exec -it dataeng-postgres bash
---
+++ dags/nft_etl.py
@@ -0,0 +1,109 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+import streamlit as st
+import http.client
+import json
+import pandas as pd
+
+# Define the default_args dictionary to specify the default parameters of the DAG
+default_args = {
+    "depends_on_past": False,
+    'owner': 'airflow',
+    'start_date': datetime(2023, 12, 16),
+}
+
+# Create a DAG instance
+dag = DAG(
+    'nft_etl',
+    default_args=default_args,
+    description='Weather ETL Pipeline to run every 15 minutes',
+    schedule_interval='*/15 * * * *'
+)
+
+def print_begin():
+    print("Begin ETL process")
+
+def run_python_script():
+    # Import your Python script here
+    conn = http.client.HTTPSConnection("streaming.bitquery.io")
+    payload = json.dumps({
+    "query": "{\n  EVM(dataset: archive) {\n    DEXTrades(\n      where: {Trade: {Dex: {ProtocolFamily: {is: \"OpenSea\"}}, Buy: {Currency: {SmartContract: {is: \"0x322e2741c792c1f2666d159bcc6d3a816f98d954\"}}}}}\n    ) {\n      Count_NFTS_bought: sum(of: Trade_Buy_Amount)\n    }\n  }\n}\n",
+    "variables": "{}"
+    })
+    headers = {
+    'Content-Type': 'application/json',
+        'Authorization': "Bearer ory_at_UAmzFEQbGw00uS2RhN-mP1zPe_VyY8KEWosxhxBEBGk.UUMF4be4c5CjEoxL935BlTsyhhphs9FJLw4Gv8Pw0Oc",
+    }
+    conn.request("POST", "/graphql", payload, headers)
+    res = conn.getresponse()
+    data = res.read()
+    print("data", data)
+    resp= json.loads( data.decode("utf-8"))
+
+    count_nfts_bought = resp['data']['EVM']['DEXTrades'][0]['Count_NFTS_bought']
+    st.title ("NFT Dashboard")
+    st.header("Punk Evil Rabbit NFT")
+    st.metric("Count of Punk Evil Rabbit NFTS Bought",count_nfts_bought)
+    payload_table = json.dumps({
+        "query": "{\n  EVM(dataset: archive, network: eth) {\n    buyside: DEXTrades(\n      limit: {count: 10}\n      orderBy: {descending: Block_Time}\n      where: {Trade: {Buy: {Currency: {SmartContract: {is: \"0x322e2741c792c1f2666d159bcc6d3a816f98d954\"}}}}}\n    ) {\n      Block {\n        Number\n        Time\n      }\n      Transaction {\n        From\n        To\n        Hash\n      }\n      Trade {\n        Buy {\n          Amount\n          Buyer\n          Currency {\n            Name\n            Symbol\n            SmartContract\n          }\n          Seller\n          Price\n        }\n        Sell {\n          Amount\n          Buyer\n          Currency {\n            Name\n            SmartContract\n            Symbol\n          }\n          Seller\n          Price\n        }\n      }\n    }\n    sellside: DEXTrades(\n      limit: {count: 10}\n      orderBy: {descending: Block_Time}\n      where: {Trade: {Buy: {Currency: {SmartContract: {is: \"0x322e2741c792c1f2666d159bcc6d3a816f98d954\"}}}}}\n    ) {\n      Block {\n        Number\n        Time\n      }\n      Transaction {\n        From\n        To\n        Hash\n      }\n      Trade {\n        Buy {\n          Amount\n          Buyer\n          Currency {\n            Name\n            Symbol\n            SmartContract\n          }\n          Seller\n          Price\n        }\n        Sell {\n          Amount\n          Buyer\n          Currency {\n            Name\n            SmartContract\n            Symbol\n          }\n          Seller\n          Price\n        }\n      }\n    }\n  }\n}\n",
+    "variables": "{}"
+    })
+
+    conn.request("POST", "/graphql", payload_table, headers)
+    res1 = conn.getresponse()
+    data1 = res1.read()
+    resp1= json.loads( data1.decode("utf-8"))
+
+    st.subheader("Latest DEX Trades")
+
+    data_table= resp1['data']['EVM']['buyside']
+    df = pd.json_normalize(data_table)
+    st.dataframe(df)
+    ## chart
+    payload3 = json.dumps({
+    "query": "{\n  EVM(dataset: archive) {\n    DEXTrades(\n      where: {Trade: {Dex: {ProtocolFamily: {is: \"OpenSea\"}}, Buy: {Currency: {SmartContract: {is: \"0x322e2741c792c1f2666d159bcc6d3a816f98d954\"}}}}}\n    ) {\n      Count_NFTS_bought: sum(of: Trade_Buy_Amount)\n      Block {\n        Date\n      }\n    }\n  }\n}\n",
+    "variables": "{}"
+    })
+
+    conn.request("POST", "/graphql", payload3, headers)
+    res3 = conn.getresponse()
+    data3 = res3.read()
+
+    chart_data=json.loads(data3)['data']['EVM']['DEXTrades']
+
+    df_chart = pd.json_normalize(chart_data)
+    df_chart.columns = ['Count_NFTS_bought', 'Block_Date']
+    # Convert the 'Count_NFTS_bought' column to integer data type
+    df_chart['Count_NFTS_bought'] = df_chart['Count_NFTS_bought'].astype(int)
+    df_chart['Block_Date'] = pd.to_datetime(df_chart['Block_Date'])
+
+    st.subheader('Daily Metrics')
+    st.line_chart(df_chart,x='Block_Date',y='Count_NFTS_bought')
+
+def print_end():
+    print("ETL process completed")
+
+
+with dag:
+    print_begin_task = PythonOperator(
+        task_id='print_begin_task',
+        python_callable=print_begin,
+        dag=dag,
+    )
+
+    execute_etl_task = PythonOperator(
+        task_id='execute_etl_task',
+        python_callable=run_python_script,
+        dag=dag,
+    )
+
+    print_end_task = PythonOperator(
+        task_id='print_end_task',
+        python_callable=print_end,
+        dag=dag,
+    )
+
+    print_begin_task >> execute_etl_task >> print_end_task
+
+
