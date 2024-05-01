d1b6765b7d937c17748a5e967cbd5202e05c2a67
nadans <nada.nisrinaseptiana@homecredit.co.id>
2024-05-02 03:19:02 +0700

    fix streamlit local

--- steamlit_app.py
+++ steamlit_app.py
@@ -11,21 +11,25 @@ def display_metric():
     for row in df.itertuples():
         st.metric("Count of Punk Evil Rabbit NFTS Bought",row.count_nfts_bought)
 
+def add_table():
+    st.subheader("Latest DEX Trades")
+    result = conn.query('SELECT * FROM buyer_table;', ttl="10m")
+    df = pd.DataFrame(result, columns=['block_number', 'block_time' , 'trade_buy_amount', 'trade_buy_buyer', 'trade_buy_currency_name', 'trade_buy_currency_smartcontract', 'trade_buy_currency_symbol', 'trade_buy_price', 'trade_buy_seller', 'trade_sell_amount', 'trade_sell_buyer', 'trade_sell_currency_name', 'trade_sell_currency_SmartContract','trade_sell_currency_symbol', 'trade_sell_price','trade_sell_seller',  'transaction_from' ,'transaction_hash','transaction_to'])
+    st.dataframe(df)
+
+def add_chart():
+    st.subheader('Daily Metrics')
+    result = conn.query('SELECT * FROM daily_metric_table;', ttl="10m")
+    df = pd.DataFrame(result, columns=['count_nfts_bought', 'block_date'])
+
+    df['block_date'] = pd.to_datetime(df['block_date'])
+    print(df)
+
+    st.line_chart(df.set_index('block_date')['count_nfts_bought'])
+
 display_metric()
+add_table()
+add_chart()
 
-# def add_table():
-#     st.subheader("Latest DEX Trades")
-#     data_table= resp1['data']['EVM']['buyside']
-#     df = pd.json_normalize(data_table)
-#     st.dataframe(df)
 
-# def add_chart():
-#     chart_data=json.loads(resp1)['data']['EVM']['DEXTrades']
-#     df_chart = pd.json_normalize(chart_data)
-#     df_chart.columns = ['Count_NFTS_bought', 'Block_Date']
-#     # Convert the 'Count_NFTS_bought' column to integer data type
-#     df_chart['Count_NFTS_bought'] = df_chart['Count_NFTS_bought'].astype(int)
-#     df_chart['Block_Date'] = pd.to_datetime(df_chart['Block_Date'])
 
-#     st.subheader('Daily Metrics')
-#     st.line_chart(df_chart,x='Block_Date',y='Count_NFTS_bought')
